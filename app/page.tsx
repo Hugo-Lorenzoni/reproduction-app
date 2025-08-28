bdc71768b80a9f8a4c1f98bc31a74c6d46a2d3fc
Hugo Lorenzoni <210937@umons.ac.be>
2025-08-28 10:02:11 +0200

    refactor: reorder console logs for clarity in environment variable output

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { env } from "process";
 
 /** Add your relevant code here for the issue to reproduce */
 export default async function Home() {
-  console.log("process.env", process.env.PHASE);
   console.log("env", env.PHASE);
+  console.log("process.env", process.env.PHASE);
   return null;
 }
